e91f542753170233ec07ac3fe3212fae92c282c4
laurynsue <66397550+laurynsue@users.noreply.github.com>
2021-06-20 02:46:02 +0800

    Add files via upload

---
+++ gerai-kita-web-app-frontend/app/register/register.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class RegisterService {
+    http: HttpClient
+    url = 'http://localhost:4000/member/register'
+
+     constructor(httpClient: HttpClient) {
+        this.http = httpClient
+     }
+
+     
+    addUsers( 
+        username: string,
+        memberName: string,
+        memberAddress: string,
+        memberImage: string,
+        email: string ,
+        memberPW: string){
+        
+        const body = {
+            username: username,
+            memberName:memberName,
+            memberAddress:memberAddress,
+            memberImage:memberImage,
+            email:email,
+            memberPW:memberPW
+        }
+        return this.http.post(this.url, body)
+    } 
+        
+}
