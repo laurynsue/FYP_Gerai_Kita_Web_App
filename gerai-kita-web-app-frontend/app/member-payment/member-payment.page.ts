4d739153752fff5d52b60267382d3926f785d839
laurynsue <66397550+laurynsue@users.noreply.github.com>
2021-06-20 03:05:07 +0800

    Add files via upload

---
+++ gerai-kita-web-app-frontend/app/member-payment/member-payment.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-member-payment',
+  templateUrl: './member-payment.page.html',
+  styleUrls: ['./member-payment.page.scss'],
+})
+export class MemberPaymentPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
