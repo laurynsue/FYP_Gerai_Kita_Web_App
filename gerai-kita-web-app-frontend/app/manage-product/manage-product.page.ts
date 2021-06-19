16da4893886e58294c8ef2765c83e4889551c3a6
laurynsue <66397550+laurynsue@users.noreply.github.com>
2021-06-20 03:03:02 +0800

    Add files via upload

---
+++ gerai-kita-web-app-frontend/app/manage-product/manage-product.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-product',
+  templateUrl: './manage-product.page.html',
+  styleUrls: ['./manage-product.page.scss'],
+})
+export class ManageProductPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
