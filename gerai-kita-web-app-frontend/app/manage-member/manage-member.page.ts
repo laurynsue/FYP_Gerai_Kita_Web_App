ae0786113daafd35d773e422b4d8fd2e92e719fe
laurynsue <66397550+laurynsue@users.noreply.github.com>
2021-06-20 03:02:15 +0800

    Add files via upload

---
+++ gerai-kita-web-app-frontend/app/manage-member/manage-member.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-member',
+  templateUrl: './manage-member.page.html',
+  styleUrls: ['./manage-member.page.scss'],
+})
+export class ManageMemberPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
