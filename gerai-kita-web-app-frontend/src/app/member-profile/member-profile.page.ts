699a3ddf039e16a113d7f81ebf51547341272667
laurynsue <66397550+laurynsue@users.noreply.github.com>
2021-06-22 08:34:25 +0800

    Add files via upload

---
+++ gerai-kita-web-app-frontend/src/app/member-profile/member-profile.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-member-profile',
+  templateUrl: './member-profile.page.html',
+  styleUrls: ['./member-profile.page.scss'],
+})
+export class MemberProfilePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
