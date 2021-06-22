802178d73f6d1541188c86186309035e42c5d8ba
laurynsue <66397550+laurynsue@users.noreply.github.com>
2021-06-22 08:26:57 +0800

    Add files via upload

---
+++ gerai-kita-web-app-frontend/src/app/login-store/login-store.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login-store',
+  templateUrl: './login-store.page.html',
+  styleUrls: ['./login-store.page.scss'],
+})
+export class LoginStorePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
