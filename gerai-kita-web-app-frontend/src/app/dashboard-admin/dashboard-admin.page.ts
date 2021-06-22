d6c1d4469b4d8746fdef746ed4547e0320a52944
laurynsue <66397550+laurynsue@users.noreply.github.com>
2021-06-22 08:18:21 +0800

    Add files via upload

---
+++ gerai-kita-web-app-frontend/src/app/dashboard-admin/dashboard-admin.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-admin',
+  templateUrl: './dashboard-admin.page.html',
+  styleUrls: ['./dashboard-admin.page.scss'],
+})
+export class DashboardAdminPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
